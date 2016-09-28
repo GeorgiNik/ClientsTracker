c9ec236b0aa1b9b9c746f23c1d22e1936da8215a
GeorgiNik <georgi.nik95@gmail.com>
2016-09-28 21:03:57 +0300

    Password policy change

--- Data/ClientsTracker.Data.Models/ApplicationUser.cs
+++ Data/ClientsTracker.Data.Models/ApplicationUser.cs
@@ -10,16 +10,8 @@
     using MysteryShopping.Data.Models.Entities;
 
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser, IDeletableEntity, IAuditInfo
+    public class ApplicationUser : IdentityUser
     {
-        public bool IsDeleted { get; set; }
-
-        public DateTime? DeletedOn { get; set; }
-
-        public DateTime CreatedOn { get; set; }
-
-        public DateTime? ModifiedOn { get; set; }
-
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
